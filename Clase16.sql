eb6b2e93340ea85452fb8d087f030aafd31fad57
franbardin <franciscobardin@gmail.com>
2017-08-07 22:50:03 -0300

    AtualizaConfianç

--- Clase16.sql
+++ Clase16.sql
@@ -41,6 +41,20 @@ SET lastName = 'Phan'
 WHERE
 employeeNumber = 1056;
 
+-- 1
+
+INSERT INTO sakila.employees (employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle)
+VALUES(0, 'Ramón', 'Don', '1983', NULL, '1', 98765, 'Mente indomable');
+-- Tira error porque el email no puede ser nulo
+
+-- 2
+
 UPDATE employee SET employeeNumber = employeeNumber - 20;
 
+--  Le quita a los empleados 20 valores de su atributo employeeNumber
+
 UPDATE employee SET employeeNumber = employeeNumber + 20;
+
+-- Tira error porque al sumarle y restarle 20 se produce que se dupliquen las primary key de ambos valores
+
+-- 3 Y desde acá no salieron :(
